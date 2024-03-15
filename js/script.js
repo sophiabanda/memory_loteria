5727510ad21c711fb1e38be31438dd4c2ccd0f7b
Sophia M Banda <51414936+sophiabanda@users.noreply.github.com>
2024-03-15 17:12:03 -0400

    added state vars and functions

--- js/script.js
+++ js/script.js
@@ -1,5 +1,55 @@
 // CONSTANTS ///////////////////////////////////////////////////
-// STATE VARS //////////////////////////////////////////////////
-// EVENT LISTENERS /////////////////////////////////////////////
-// FUNCTIONS  //////////////////////////////////////////////////
+const cardOptions = [
+    {img: ''},
+    {img: '', matched: false},
+    {img: '', matched: false},
+    {img: '', matched: false},
+    {img: '', matched: false},
+    {img: '', matched: false},
+    {img: '', matched: false},
+    {img: '', matched: false},
+]
+
+// STATE VARS (MODEL) //////////////////////////////////////////////////
+let numberOfGuesses, gameTimer
+
 // CACHED ELS //////////////////////////////////////////////////
+    //start button
+
+// EVENT LISTENERS /////////////////////////////////////////////
+
+
+// FUNCTIONS  //////////////////////////////////////////////////
+initialize();
+
+function initialize() {
+    // reset all div/img's to back of card image
+    // clear timer
+    // reset # of guesses
+}
+
+function handleSelection() {
+
+}
+
+function compareChoices() {
+
+}
+
+function startGame() {
+    // cards are randomized
+    // on start button click, all cards flip over for 1.5sec
+    // card flipping/choosing is enabled
+
+    // render timer
+    // render # of guesses
+    render();
+}
+
+function render() {
+    handleSelection();
+    compareChoices();
+}
+
+
+
