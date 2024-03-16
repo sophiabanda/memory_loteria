125677651724395dea224b5b051dcb677856a870
Sophia M Banda <51414936+sophiabanda@users.noreply.github.com>
2024-03-16 18:19:37 -0400

    refactored shuffle algorithm

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
 // CONSTANTS ///////////////////////////////////////////////////
 const cardOptions = [
-  { img: '/css/images/borracho.jpg' },
+  { img: '/css/images/corazon.jpg' },
   { img: '/css/images/borracho.jpg' },
   { img: '/css/images/corazon.jpg' },
-  { img: '/css/images/corazon.jpg' },
-  { img: '/css/images/gallo.jpg' },
+  { img: '/css/images/selena_cantante.jpg' },
   { img: '/css/images/gallo.jpg' },
   { img: '/css/images/luna.jpg' },
+  { img: '/css/images/gallo.jpg' },
   { img: '/css/images/luna.jpg' },
   { img: '/css/images/nopal.jpg' },
-  { img: '/css/images/nopal.jpg' },
-  { img: '/css/images/paraguas.jpg' },
-  { img: '/css/images/paraguas.jpg' },
-  { img: '/css/images/selena_cantante.jpg' },
-  { img: '/css/images/selena_cantante.jpg' },
   { img: '/css/images/escalera.jpg' },
+  { img: '/css/images/borracho.jpg' },
+  { img: '/css/images/paraguas.jpg' },
+  { img: '/css/images/nopal.jpg' },
+  { img: '/css/images/paraguas.jpg' },
+  { img: '/css/images/selena_cantante.jpg' },
   { img: '/css/images/escalera.jpg' },
   // each of the card options will be used twice and shuffled
   // so there needs to be two of each in order to not modify the original object
@@ -46,8 +46,8 @@ initialize();
 function initialize() {
   shuffleCards();
   cardOptions.forEach((imgOption, idx) => {
-    shuffleCards();
     cards[idx].setAttribute('src', imgOption.img);
+    // is there a way to set the alt att dynamically, too?
   });
 
   render();
@@ -56,23 +56,14 @@ function initialize() {
 function shuffleCards() {
   //Fisher-Yates sorting algo
   for (let i = cardOptions.length - 1; i > 0; i--) {
-    // let i = second to last item in array
-    // while i > 0, minus length, going back thru array
     const j = Math.floor(Math.random() * (i + 1));
-    // randomizing array index + 1
-    const temporary = cardOptions[i];
-    // temp array index is the array idx
-    cardOptions[i] = cardOptions[j];
-    // change array idx to newly shuffled array idx
-    cardOptions[j] = temporary;
-    // process cycles back over
+    [cardOptions[i], cardOptions[j]] = [cardOptions[j], cardOptions[i]];
   }
-  console.log(cardOptions);
   return cardOptions;
 }
 
 function handleSelection(event) {
-  console.log(event);
+  console.log(event.target.id);
 }
 
 function compareChoices() {}
