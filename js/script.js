8a27481ee329198710230bece71f27bbdf3ffd4f
Sophia M Banda <51414936+sophiabanda@users.noreply.github.com>
2024-03-18 20:31:18 -0400

    WIP comparison logic

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // CONSTANTS ///////////////////////////////////////////////////
 const cardOptions = [
-  { img: '/css/images/corazon.jpg', id: c4, hidden: true },
+  { img: '/css/images/corazon.jpg', hidden: true },
   { img: '/css/images/borracho.jpg', hidden: true },
   { img: '/css/images/corazon.jpg', hidden: true },
   { img: '/css/images/selena_cantante.jpg', hidden: true },
@@ -24,44 +24,41 @@ const backOfCard = '/css/images/mexican_blanket.jpg';
 // STATE VARS (MODEL) //////////////////////////////////////////////////
 let cardOne = null;
 let cardTwo = null;
-
+let mixedDeck;
 // CACHED ELS //////////////////////////////////////////////////
 const cards = [...document.querySelectorAll('img')];
 const container = document.getElementById('container');
 
 // EVENT LISTENERS /////////////////////////////////////////////
-container.addEventListener('click', handleSelection);
+// container.addEventListener('click', handleSelection);
 cards.forEach((card) => {
   card.addEventListener('click', handleFlip);
 });
 // FUNCTIONS  //////////////////////////////////////////////////
-// initialize();
-// reset all div/img's to back of card image
-// clear timer
-// reset # of guesses
-// cards are randomized
-// on start button click, all cards flip over for 1.5sec
-// card flipping/choosing is enabled
 
-// render timer
-// render # of guesses
 initialize();
 
 function initialize() {
   shuffleCards();
+  mixedDeck = [];
   cardOptions.forEach((imgOption, idx) => {
-    if (imgOption.hidden) {
+    imgOption.id = idx;
+    cards[idx].setAttribute('src', imgOption.img);
+    if (imgOption.hidden === true) {
       cards[idx].setAttribute('src', backOfCard);
     } else {
       cards[idx].setAttribute('src', imgOption.img);
     }
-    // console.log('imgOp', imgOption, 'idx', idx);
-    // is there a way to set the alt att dynamically, too?
   });
-  console.log(cardOptions);
+  console.log();
   handleSelection();
 }
 
+// if (imgOption.hidden) {
+//   cards[idx].setAttribute('src', backOfCard);
+// } else {
+//   cards[idx].setAttribute('src', imgOption.img);
+// }
 function startGame() {
   //start timer
 }
@@ -76,15 +73,19 @@ function shuffleCards() {
 }
 
 function handleFlip(event) {
-  console.log(event.target.id);
-
-  // Use .find method on cardOptions
-  // for each card, check if card's id === event.target.id
-  // if so, hidden = false
+  let clickedCard = event.target.id;
+  cardOptions.find((card) => {
+    let id = card.id;
+    let stringId = String(id);
+    if (`c${stringId}` === clickedCard) {
+      card.hidden = false;
+      card.img = event.target;
+    }
+  });
 }
 
 function handleSelection(event) {
-  if (event.target.tagName !== 'IMG') return;
+  if (!event || event.target.tagName !== 'IMG') return;
   if (cardOne === null) {
     cardOne = event.target.src;
     // console.log(cardOne);
