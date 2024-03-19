ea6c2e2d91caf303d4c7ae5123f6d0ba37029d60
Sophia M Banda <51414936+sophiabanda@users.noreply.github.com>
2024-03-18 21:00:52 -0400

    wip, handle flip

--- js/script.js
+++ js/script.js
@@ -30,7 +30,7 @@ const cards = [...document.querySelectorAll('img')];
 const container = document.getElementById('container');
 
 // EVENT LISTENERS /////////////////////////////////////////////
-// container.addEventListener('click', handleSelection);
+container.addEventListener('click', handleSelection);
 cards.forEach((card) => {
   card.addEventListener('click', handleFlip);
 });
@@ -50,15 +50,9 @@ function initialize() {
       cards[idx].setAttribute('src', imgOption.img);
     }
   });
-  console.log();
   handleSelection();
 }
 
-// if (imgOption.hidden) {
-//   cards[idx].setAttribute('src', backOfCard);
-// } else {
-//   cards[idx].setAttribute('src', imgOption.img);
-// }
 function startGame() {
   //start timer
 }
@@ -73,25 +67,24 @@ function shuffleCards() {
 }
 
 function handleFlip(event) {
-  let clickedCard = event.target.id;
+  let clickedCard = event.target.id.slice(1);
   cardOptions.find((card) => {
     let id = card.id;
-    let stringId = String(id);
-    if (`c${stringId}` === clickedCard) {
+    if (id === clickedCard) {
       card.hidden = false;
-      card.img = event.target;
     }
+    console.log(card.hidden);
   });
 }
 
 function handleSelection(event) {
-  if (!event || event.target.tagName !== 'IMG') return;
+  if (event.target.tagName !== 'IMG') return;
   if (cardOne === null) {
     cardOne = event.target.src;
-    // console.log(cardOne);
+    console.log(cardOne);
   } else {
     cardTwo = event.target.src;
-    // console.log(cardTwo);
+    console.log(cardTwo);
     compareChoices();
   }
 }
