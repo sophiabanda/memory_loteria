9968c80df0eb9f861f55eabb06251330c1ccddb7
Sophia M Banda <51414936+sophiabanda@users.noreply.github.com>
2024-03-20 10:27:38 -0400

    RESTRUCTURE of CSS file

--- js/script.js
+++ js/script.js
@@ -1,24 +1,24 @@
 // ------ CONSTANTS -------------------------------------------------------------------
 const cardOptions = [
-  { img: '/css/images/corazon.jpg', hidden: true },
-  { img: '/css/images/borracho.jpg', hidden: true },
-  { img: '/css/images/corazon.jpg', hidden: true },
-  { img: '/css/images/selena_cantante.jpg', hidden: true },
-  { img: '/css/images/gallo.jpg', hidden: true },
-  { img: '/css/images/luna.jpg', hidden: true },
-  { img: '/css/images/gallo.jpg', hidden: true },
-  { img: '/css/images/luna.jpg', hidden: true },
-  { img: '/css/images/nopal.jpg', hidden: true },
-  { img: '/css/images/escalera.jpg', hidden: true },
-  { img: '/css/images/borracho.jpg', hidden: true },
-  { img: '/css/images/paraguas.jpg', hidden: true },
-  { img: '/css/images/nopal.jpg', hidden: true },
-  { img: '/css/images/paraguas.jpg', hidden: true },
-  { img: '/css/images/selena_cantante.jpg', hidden: true },
-  { img: '/css/images/escalera.jpg', hidden: true },
+  { img: '/css/assets/corazon.jpg', hidden: true },
+  { img: '/css/assets/borracho.jpg', hidden: true },
+  { img: '/css/assets/corazon.jpg', hidden: true },
+  { img: '/css/assets/selena_cantante.jpg', hidden: true },
+  { img: '/css/assets/gallo.jpg', hidden: true },
+  { img: '/css/assets/luna.jpg', hidden: true },
+  { img: '/css/assets/gallo.jpg', hidden: true },
+  { img: '/css/assets/luna.jpg', hidden: true },
+  { img: '/css/assets/nopal.jpg', hidden: true },
+  { img: '/css/assets/escalera.jpg', hidden: true },
+  { img: '/css/assets/borracho.jpg', hidden: true },
+  { img: '/css/assets/paraguas.jpg', hidden: true },
+  { img: '/css/assets/nopal.jpg', hidden: true },
+  { img: '/css/assets/paraguas.jpg', hidden: true },
+  { img: '/css/assets/selena_cantante.jpg', hidden: true },
+  { img: '/css/assets/escalera.jpg', hidden: true },
   // each of the card options will be used twice and shuffled
 ];
-const backOfCard = '/css/images/mexican_blanket.jpg';
+const backOfCard = '/css/assets/mexican_blanket.jpg';
 // console.log(cardOptions[5].img);
 
 // ------ STATE VARS (MODEL) -----------------------------------------------------------
@@ -31,28 +31,42 @@ const cards = [...document.querySelectorAll('img')];
 const container = document.getElementById('container');
 const guessContainer = document.getElementById('guesses');
 let bodyContainer = document.querySelector('body');
-let resetButton;
 
 // ------ EVENT LISTENERS --------------------------------------------------------------
 cards.forEach((card) => {
   card.addEventListener('click', handleFlipSelection);
 });
+
+document
+  .querySelector('button')
+  .addEventListener('click', () => initialize(true));
 // ------ FUNCTIONS  -------------------------------------------------------------------
 
 initialize();
-function initialize() {
-  numOfGuesses = 6;
+
+function initialize(reset) {
+  numOfGuesses = 10;
   guessContainer.innerText = `# of Guesses: ${numOfGuesses}`;
   shuffleCards();
-  cardOptions.forEach((imgOption, idx) => {
-    imgOption.id = idx;
-    cards[idx].setAttribute('src', imgOption.img);
-    if (imgOption.hidden === true) {
+  if (reset) {
+    shuffleCards();
+    cards.hidden = true;
+    cardOptions.forEach((imgOption, idx) => {
       cards[idx].setAttribute('src', backOfCard);
-    } else {
+      cards[idx].classList.remove('no-click');
+      cards[idx].classList.remove('fade-out');
+    });
+  } else {
+    cardOptions.forEach((imgOption, idx) => {
+      imgOption.id = idx;
       cards[idx].setAttribute('src', imgOption.img);
-    }
-  });
+      if (imgOption.hidden === true) {
+        cards[idx].setAttribute('src', backOfCard);
+      } else {
+        cards[idx].setAttribute('src', imgOption.img);
+      }
+    });
+  }
 }
 
 function shuffleCards() {
@@ -77,9 +91,8 @@ function handleFlipSelection(event) {
     cardTwo = cards[clickedCardId];
     cards[clickedCardId].classList.add('no-click');
   }
-  // console.log('clickedCardId=>', clickedCardId, 'foundCard=>', foundCard);
-  console.log('cardOne=>', cardOne, 'cardTwo=>', cardTwo);
   compareChoices();
+  checkForWin();
 }
 
 function compareChoices() {
@@ -89,14 +102,15 @@ function compareChoices() {
     console.log(`It's a celebration!`); // maybe as sound plays?
   } else {
     console.log(`No match!`); // maybe a sad sound plays
-    setTimeout(flipBack, 1000);
     cardOne.classList.remove('no-click');
     cardTwo.classList.remove('no-click');
+    setTimeout(flipBack, 1000);
   }
-  setTimeout(clearCards, 1150);
+  setTimeout(clearCards, 1003);
 }
 
 function flipBack() {
+  console.log(`I just ran`);
   cardOne.src = backOfCard;
   cardTwo.src = backOfCard;
   numOfGuesses--;
@@ -107,6 +121,8 @@ function flipBack() {
 }
 
 function clearCards() {
+  console.log(`I just ran`);
+
   cardOne = null;
   cardTwo = null;
 }
@@ -115,7 +131,16 @@ function loseGame() {
   guessContainer.innerText = `Nooo... Would you like to try again?`;
   cards.forEach((card) => {
     card.classList.add('fade-out');
+    card.classList.add('no-click');
   });
+}
+
+function checkForWin() {
+  if (cardOptions.some((card) => card.hidden === true)) {
+    return;
+  } else {
+    winGame();
+  }
 }
 
 function winGame() {
@@ -123,6 +148,6 @@ function winGame() {
     card.classList.add('fade-out');
   });
   bodyContainer.classList.add('fade-in');
-  bodyContainer.style.backgroundImage = 'url(css/images/papel_picado.jpeg)';
+  bodyContainer.style.backgroundImage = 'url(css/assets/papel_picado.jpeg)';
   container.innerHTML = '<h1>YOU WON!!!</H1>';
 }
